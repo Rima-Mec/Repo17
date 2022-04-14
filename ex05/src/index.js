a26d9e3ca42aa5f21520bd3b0d08f169828df060
Rima-Mec <dmerima08@gmail.com>
2022-04-14 10:18:16 +0200

    Adding index.js file

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+const [foo, boo] = [12, 45];
+function main (foo, boo) {
+
+// only change code below this line
+
+// only change code above this line
+return {foo, boo}
+}
+console.log(main(foo, boo)); // [45, 12]
+module.exports = main;
